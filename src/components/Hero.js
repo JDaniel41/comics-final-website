f65024cfa10e66ed52f3d06851736c5dd6fee1cc
Jonathan Daniel <gcagrad19@gmail.com>
2022-12-01 18:10:01 -0500

    First Build?

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function Hero() {
     return (
-        <div>
-            <h1>Hero</h1>
+        <div className="bg-orange-100 h-screen flex">
+            <div className="m-auto">
+                <h1 className="text-9xl text-center m-2">Divination Dilemma</h1>
+                <h2 className="text-6xl text-center m-2">
+                    By Christian and Jonathan!
+                </h2>
+            </div>
         </div>
     );
 }
