e65107a1cb3ea09a8ffd0a3cd5aeb80a206ba12b
Jonathan Daniel <gcagrad19@gmail.com>
2022-12-01 17:56:28 -0500

    First Shot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Hero from "./components/Hero";
+import Intro from "./components/Intro";
+
 function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <div>
+            <Hero />
+            <Intro />
+        </div>
+    );
 }
 
 export default App;
